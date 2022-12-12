b8a2dc350b0c762c35e06680124ec67c4d472db6
Evgenii Elchev <yolchev@gmail.com>
2022-12-12 11:24:15 +0700

    d8

--- src/d8.kt
+++ src/d8.kt
@@ -2,12 +2,12 @@ fun main() {
     fun part1(input: List<String>): Int = input.flatMap { it.trim().split("").drop(1).dropLast(1).map { it.toInt() } }
             .chunked(input.size * input.size).map { matrix ->
                 matrix.foldIndexed(0) { index, acc, item ->
-                    if (index / input.size == 0 || index / input.size == input.size - 1 || index % input.size == 0 || index % input.size == input.size - 1) {
-                        acc + 1
-                    } else if ((index / input.size * input.size until index).map { matrix[it] }.any { it >= item  }.not()
-                            || (index + 1 until index / input.size * input.size + input.size).map { matrix[it] }.any { it >= item  }.not()
-                            || (index - index / input.size * input.size until index step input.size).map { matrix[it] }.any { it >= item  }.not()
-                            || (index + input.size .. (input.size - 1 - index / input.size) * input.size + index % input.size + index step input.size).map { matrix[it] }.any { it >= item  }.not() ) {
+                    if (
+                            (index / input.size == 0 || index / input.size == input.size - 1 || index % input.size == 0 || index % input.size == input.size - 1)
+                            || ((index / input.size * input.size until index).map { matrix[it] }.any { it >= item  }.not()
+                                || (index + 1 until index / input.size * input.size + input.size).map { matrix[it] }.any { it >= item  }.not()
+                                || (index - index / input.size * input.size until index step input.size).map { matrix[it] }.any { it >= item  }.not()
+                                || (index + input.size .. (input.size - 1 - index / input.size) * input.size + index % input.size + index step input.size).map { matrix[it] }.any { it >= item  }.not() )) {
                         acc + 1
                     } else {
                         acc
